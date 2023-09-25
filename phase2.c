19da656467dbce6eefd2eb912088fef3feedef4d
Grace Zhang <gracezhang@dhcp-10-134-164-185.uawifi.arizona.edu>
2023-09-25 12:23:17 -0700

    add phase2.c file

---
+++ phase2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include "phase2.h"
+
+void phase2_init(void) {
+
+}
+
+void phase2_start_service_processes(void) {
+
+}
+
+int phase2_check_io(void) {
+
+}
+
+void phase2_clockHandler(void) {
+
+}
+
+int MboxCreate(int slots, int slot_size) {
+
+}
+
+int MboxRelease(int mbox_id) {
+
+}
+
+int MboxSend(int mbox_id, void *msg_ptr, int msg_size) {
+
+}
+
+int MboxCondSend(int mbox_id, void *msg_ptr, int msg_size) {
+
+}
+
+int MboxRecv(int mbox_id, void *msg_ptr, int msg_max_size) {
+
+}
+
+int MboxCondRecv(int mbox_id, void *msg_ptr, int msg_max_size) {
+
+}
+
+void waitDevice(int type, int unit, int *status) {
+
+}
+
+void wakeupByDevice(int type, int unit, int status) {
+
+}
+
+
+
+
+
